36c24a4641b670f66e0513e00e6a37b8c8dba06f
Igzario <88246005+Igzario@users.noreply.github.com>
2023-08-25 19:57:20 +0300

    15tz - 4st commit after check

--- src/test/java/ru/practicum/shareit/repositoryTest/ItemRequestRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/repositoryTest/ItemRequestRepositoryTest.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.repositoryTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
