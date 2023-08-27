a3faecf1cf7f7ac925fdbd8328a1599d413ff813
Igzario <88246005+Igzario@users.noreply.github.com>
2023-08-27 18:41:54 +0300

    16tz - v1

--- server/src/test/java/ru/practicum/shareit/repositoryTest/UserRepositoryTest.java
+++ server/src/test/java/ru/practicum/shareit/repositoryTest/UserRepositoryTest.java
@@ -2,6 +2,8 @@ package ru.practicum.shareit.repositoryTest;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
